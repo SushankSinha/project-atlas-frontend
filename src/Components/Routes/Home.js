018f14304159ab2ea512df8e60de00f81509c459
SushankSinha <SushankSinha>
2023-10-05 03:01:21 +0530

    Axios Update

--- src/Components/Routes/Home.js
+++ src/Components/Routes/Home.js
@@ -4,13 +4,14 @@ import teamwork from '../Authentication/Photos/teamwork.jpg'
 import meeting from '../Authentication/Photos/meeting.jpg'
 import Typography from "@mui/material/Typography";
 import api from '../api';
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
+// import {useCookies} from 'react-cookie'
 
 function Home() {
 
   const [data, setData] = useState(null);
 
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
 /* eslint-disable react-hooks/exhaustive-deps */
   
@@ -20,11 +21,10 @@ async function LoadHome(){
         const response = await api.get('/');
     if(response.status === 200){
     setData(response.data)
+    console.log(response.data)
     } 
       }catch(error){
-        if(error){
-          navigate('/login')
-        }
+        console.log(error)
     }
     };
   
@@ -32,40 +32,33 @@ async function LoadHome(){
       LoadHome()
   }, []);
 
-  function HomePage (){
-    return (
-      <div >
-      <Typography style = {{margin: '2%', fontWeight : 'bold'}} >
-        <h1 align = { 'center'}> Welcome back </h1>
-      </Typography>
-        <div style = {{margin: '5% auto', display : 'block'}} id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
-      <div style = {{height : '100%', width: '100%'}} className="carousel-inner">
-        <div  className="carousel-item active">
-          <img  src={timeline} className="d-block w-100" alt="timeline" />
-        </div>
-        <div  className="carousel-item">
-          <img  src={meeting} className="d-block w-100" alt="meeting" />
-        </div>
-        <div  className="carousel-item">
-          <img  src={teamwork} className="d-block w-100" alt="team work" />
-        </div>
-      </div>
-      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Previous</span>
-      </button>
-      <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Next</span>
-      </button>
-    </div>
-      </div>
-    )
-  }
 
   return (
-    <div>
-    {data && (<HomePage/>)}
+    <div >
+    <Typography style = {{margin: '2%', fontWeight : 'bold'}} >
+      <h1 align = { 'center'}> Welcome back </h1>
+    </Typography>
+      <div style = {{margin: '5% auto', display : 'block'}} id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
+    <div style = {{height : '100%', width: '100%'}} className="carousel-inner">
+      <div  className="carousel-item active">
+        <img  src={timeline} className="d-block w-100" alt="timeline" />
+      </div>
+      <div  className="carousel-item">
+        <img  src={meeting} className="d-block w-100" alt="meeting" />
+      </div>
+      <div  className="carousel-item">
+        <img  src={teamwork} className="d-block w-100" alt="team work" />
+      </div>
+    </div>
+    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Previous</span>
+    </button>
+    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+      <span className="carousel-control-next-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Next</span>
+    </button>
+  </div>
     </div>
   )
 }
